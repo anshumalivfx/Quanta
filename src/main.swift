6da5c677e779571aca4e1b631dd7b123f195d36e
Anshumali Karna <anshumali.karna99@gmail.com>
2024-08-25 17:06:10 +0530

    generatedCard

--- src/main.swift
+++
@@ -1,8 +0,0 @@
-var lexer: LEXER = init_lexer(content: "let x = \"hello\";")
-
-
-var token: TOKEN
-repeat {
-    token = lexer_get_next_token(lexer: &lexer)
-    print(token)
-} while token.type != TokenType.TOKEN_EOF
